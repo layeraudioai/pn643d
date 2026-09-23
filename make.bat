cf261e720f2142c94e8c7e2df53e5794c85a4b65
xbcx <murderlandsgame@gmail.com>
2026-09-23 03:09:16 -0400

    make

--- make.bat
+++ make.bat
@@ -1,12 +1,22 @@
-set curdir=%CD%
+@ECHO OFF
+set curdir="%CD%"
+echo compile and install dependencies
 cd picaGL && make && make install && cd ../imgui-picagl && make && make install && cd ../DaedalusX64-3DS
+echo next step
+pause
+echo here wii go
 FOR %%I in (..\roms\*.*64) DO (
-  copy %%I Source\SysCTR\Resources\romfs
-  tools\ctrtool --romfsdir Source\SysCTR\Resources\romfs Source\SysCTR\Resources\romfs.bin
+  del daedbuild\CMakeCache.txt
+  echo %%~nI
+  copy "%%I" "Source\SysCTR\Resources\romfs"
+  pause
+  echo > "Source\SysCTR\Resources\romfs.bin"
+  tools\3dstool -c --romfs-dir "Source\SysCTR\Resources\romfs" "Source\SysCTR\Resources\romfs.bin"
   sh build_daedalus.sh CTR_RELEASE
-  copy daedbuild\DaedalusX64.cia dist\%%~nI.cia
-  mkdir dist\3ds\%%~nI
-  copy daedbuild\DaedalusX64.3dsx dist\3ds\%%~nI\%%~nI.3dsx
-  del Source\SysCTR\Resources\romfs\%%~nI
+  copy "daedbuild\DaedalusX64.cia" "dist\%%~nI.cia"
+  mkdir "dist\3ds\%%~nI"
+  copy "daedbuild\DaedalusX64.3dsx" "dist\3ds\%%~nI\%%~nI.3dsx"
+  del "Source\SysCTR\Resources\romfs\%%~nI"
+  echo %%~nI done
 )
-cd %curdir%
+cd "%curdir%"

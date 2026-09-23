b02d4bb837cfa9753044d7b6df5972ba8593f80e
xbcx <murderlandsgame@gmail.com>
2026-09-23 02:32:44 -0400

    make script update

--- make.bat
+++ make.bat
@@ -1,3 +1,12 @@
 set curdir=%CD%
-cd picaGL && make && make install && cd ../imgui-picagl && make && make install && cd ../DaedalusX64-3DS && sh build_daedalus.sh CTR_RELEASE && cd ..
+cd picaGL && make && make install && cd ../imgui-picagl && make && make install && cd ../DaedalusX64-3DS
+for %%I in (../*/*.*64) (
+  copy %%I Source\SysCTR\Resources\romfs
+  tools\ctrtool --romfsdir Source\SysCTR\Resources\romfs Source\SysCTR\Resources\romfs.bin
+  sh build_daedalus.sh CTR_RELEASE
+  copy daedbuild\DaedalusX64.cia dist\%~nI.cia
+  mkdir dist\3ds\%~n
+  copy daedbuild\DaedalusX64.3dsx dist\3ds\%~n\%~nI.3dsx
+  del Source\SysCTR\Resources\%~nI
+)
 cd %curdir%
